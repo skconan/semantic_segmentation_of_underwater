7ea19f57071144d0fd4ff19e764899c6a317d9e4
skconan <supakit.kr@gmail.com>
2019-04-19 00:59:16 +0700

    add const and utilities

---
+++ source/utilities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+    File name: utilities.py
+    Author: skconan
+    Date created: 2019/04/09
+    Python Version: 3.6
+"""
+
+import os
+
+
+def get_file_path(dir_name):
+    file_list = os.listdir(dir_name)
+    files = []
+    for f in file_list:
+        abs_path = os.path.join(dir_name, f)
+        if os.path.isdir(abs_path):
+            files = files + get_file_path(abs_path)
+        else:
+            files.append(abs_path)
+
+    return files
+
+def get_file_name(img_path):
+    name = img_path.split('\\')[-1]
+    name = name.replace('.gif', '')
+    name = name.replace('.png', '')
+    name = name.replace('.jpg', '')
+    return name
+
